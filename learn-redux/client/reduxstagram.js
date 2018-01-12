195ed5c61788368c22cb00e8b6b320dcef989ae9
Peter Banjo (desktop) <peter.banjo@teccura.software>
2018-01-12 15:45:00 +0000

    create PhotoGrid and single components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,5 +3,19 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const app = (
+  <div>
+    <Main />
+    <Router history={browserHistory}>
+      <Route path="/" component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Router>
+  </div>
+)
+
+render(app, document.getElementById('root'));
